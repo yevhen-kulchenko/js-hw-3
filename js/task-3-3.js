94dcab8dd0c9ebc88b4e70226931349f36355953
yevhen kulchenko <evhenkulchenko@gmail.com>
2020-11-14 11:23:08 +0200

    Finished 3-rd module

---
+++ js/task-3-3.js
@@ -0,0 +1,42 @@
+const findBestEmployee = function (employees) {
+  'use strict';
+  // Write code under this line
+  const keys = Object.keys(employees);
+  let bigerNumber = 0;
+  let bestEmployee = '';
+
+  for (const key of keys) {
+    if (employees[key] > bigerNumber) {
+      bigerNumber = employees[key];
+      bestEmployee = key;
+    }
+  }
+  return bestEmployee;
+};
+
+// Объекты и ожидаемый результат
+const developers = {
+  ann: 29,
+  david: 35,
+  helen: 1,
+  lorence: 99,
+};
+console.log(findBestEmployee(developers));
+// 'lorence'
+
+const supports = {
+  poly: 12,
+  mango: 17,
+  ajax: 4,
+};
+console.log(findBestEmployee(supports));
+// 'mango'
+
+const sellers = {
+  lux: 147,
+  david: 21,
+  kiwi: 19,
+  chelsy: 38,
+};
+console.log(findBestEmployee(sellers));
+// 'lux'
